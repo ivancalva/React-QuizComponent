d3b39fe84c5bfc0acb342e15715682539178fe7f
Ivan Calva <luis.calva@unosquare.com>
2018-09-14 11:19:43 -0500

    Modulo 3

---
+++ src/QuizQuestionButton.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class QuizQuestionButton extends Component {
+    render() {
+        return(
+            <l1><button>{this.props.button_text}</button></l1>
+        )
+    }
+}
+
+export default QuizQuestionButton
