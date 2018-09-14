03136e438b1a97fb5f0ad6f6cbdd678d54261eda
Ivan Calva <luis.calva@unosquare.com>
2018-09-14 11:56:06 -0500

    Error de mensaje

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -2,13 +2,21 @@ import React, { Component } from 'react'
 import QuizQuestionButton from './QuizQuestionButton'
 
 class QuizQuestion extends Component {
+    constructor(props){
+        super(props)
+        this.state = { incorrectAnswer: false }
+    }
+
     handleClick(buttonText) {
         if(buttonText === this.props.quiz_question.answer) {
             this.props.showNextQuestionHandler()
+            this.setState((state) =>{
+                return { incorrectAnswer : false }
+            })
         }
-    }
-    showNextQuestion() {
-
+        this.setState((state) =>{
+            return { incorrectAnswer : true }
+        })
     }
     render() {
         return(
@@ -21,10 +29,11 @@ class QuizQuestion extends Component {
                     {this.props.quiz_question.answer_options.map((answer_option, index) =>{
                         return <QuizQuestionButton key={index} button_text={answer_option} 
                         clickHandler={this.handleClick.bind(this)}
-                        showNextQuestionHandler={this.showNextQuestion.bind(this)}/>
+                        />
                     })}
                 </ul>
                 </section>
+                {this.state.incorrectAnswer ? <p className="error">Sorry, that's not right</p> : null}
             </main>
         )
     }
